d33062c2bd55a569715cb884e307f84eae274a45
he00741098 <86698067+he00741098@users.noreply.github.com>
2023-02-06 23:08:11 +0000

    pixi.js

---
+++ node_modules/@pixi/settings/lib/settings.mjs
@@ -0,0 +1,24 @@
+import { BrowserAdapter } from './adapter.mjs';
+
+const settings = {
+  ADAPTER: BrowserAdapter,
+  RESOLUTION: 1,
+  RENDER_OPTIONS: {
+    view: null,
+    width: 800,
+    height: 600,
+    autoDensity: false,
+    backgroundColor: 0,
+    backgroundAlpha: 1,
+    clearBeforeRender: true,
+    antialias: false,
+    premultipliedAlpha: true,
+    preserveDrawingBuffer: false,
+    hello: false
+  },
+  CREATE_IMAGE_BITMAP: false,
+  ROUND_PIXELS: false
+};
+
+export { settings };
+//# sourceMappingURL=settings.mjs.map
