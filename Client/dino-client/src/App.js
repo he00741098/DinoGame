bb3bbaaa3e368c1c860c1c6aa140230c0677db8c
DebianProgrammer <34977208+DebianProgrammer@users.noreply.github.com>
2023-02-06 15:07:33 -0800

    Modify client

--- Client/dino-client/src/App.js
+++ Client/dino-client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './dino.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Multiplayer Chrome Dino!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/games">Join the global games</a>
       </header>
     </div>
   );
