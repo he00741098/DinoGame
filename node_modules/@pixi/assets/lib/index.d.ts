1a5081ee4814c390e6dd2ea5b1d2e81feee29eea
DebianProgrammer <34977208+DebianProgrammer@users.noreply.github.com>
2023-02-08 14:16:25 -0800

    rewrite

--- node_modules/@pixi/assets/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './AssetExtension';
-export * from './Assets';
-export * from './cache';
-export * from './detections';
-export * from './loader';
-export * from './resolver';
-export * from './utils';
