16692e3d3a5c3cbfa30255f9f730f26a8e1ad859
he00741098 <86698067+he00741098@users.noreply.github.com>
2023-02-11 23:47:47 +0000

    Added movable circle, made dino image work

--- client/client.js
+++ client/client.js
@@ -1,3 +1,4 @@
+//import * as TWEEDLE from TWEEDLE;
 const container = document.getElementById("container");
 
 const image = new Image();
@@ -6,12 +7,12 @@ image.src = '/images/DinoSprites.png';
 const atlasData = {
 	frames: {
 		Dino1: {
-			frame: { x: 0, y:0, w:32, h:32 },
+			frame: { x: 0, y:0, w:60, h:60 },
 			sourceSize: { w: 32, h: 32 },
 			spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
 		},
 		Dino2: {
-			frame: { x: 32, y:0, w:32, h:32 },
+			frame: { x: 60, y:0, w:60, h:60 },
 			sourceSize: { w: 32, h: 32 },
 			spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
 		},
@@ -37,13 +38,106 @@ spritesheet.parse();
 const anim = new PIXI.AnimatedSprite(spritesheet.animations.dino);
 anim.animationSpeed = 0.1666;
 anim.play();
-
+let app = new PIXI.Application({width: 1106});
+app.renderer.backgroundColor = 0x456268;
 
 window.onload = function (){
-    let app = new PIXI.Application({width: 1106});
+    //let app = new PIXI.Application({width: 1106});
     container.appendChild(app.view);
     anim.anchor.set(0.5);
-    anim.x = app.view.width / 2;
+    anim.x = app.view.width / 6;
     anim.y = app.view.height / 2;
     app.stage.addChild(anim);
+
+
 }
+
+function gameLoop() {
+    player.update();
+
+}
+
+let w = 512, h=512;
+let pressed = {};
+class Circle {
+    constructor(color, radius, v) {
+        this.radius = radius;
+        this.v = v;
+
+        let circle = new PIXI.Graphics();
+        circle.beginFill(color);
+        circle.drawCircle(0, 0, radius);
+        circle.endFill();
+        circle.x = radius;
+        circle.y = radius;
+        app.stage.addChild(circle);
+
+        this.circle = circle;
+    }
+}
+
+
+class Player extends Circle {
+    constructor(color, radius, v) {
+        super(color, radius, v);
+        this.reset();
+    }
+
+    reset() {
+        this.circle.x = w/2;
+        this.circle.y = h/2;
+        this.speed = 2;
+    }
+
+    update() {
+        let x = this.circle.x + this.v.x;
+        let y = this.circle.y + this.v.y;
+
+        this.circle.x = Math.min(Math.max(x, this.radius), w-this.radius);
+        this.circle.y = Math.min(Math.max(y, this.radius), w-this.radius);
+    }
+}
+
+function onkeydown(ev) {
+    switch (ev.key) {
+
+
+        case "ArrowUp":
+        case "w":
+            player.v.y = -player.speed;
+            pressed['up'] = true;
+            break;
+
+        case "ArrowDown": 
+        case "s":
+            player.v.y = player.speed;
+            pressed['down'] = true;
+            break;
+    }
+}
+function onkeyup(ev) {
+    switch (ev.key) {
+
+        case "ArrowUp": 
+        case "w":
+            player.v.y = pressed['down']?player.speed:0; 
+            pressed['up'] = false;
+            break;
+
+        case "ArrowDown": 
+        case "s":
+            player.v.y = pressed['up']?-player.speed:0; 
+            pressed['down'] = false;
+            break;
+    }
+}
+
+function setupControls() {
+    window.addEventListener("keydown", onkeydown);
+    window.addEventListener("keyup", onkeyup);
+}
+
+//---
+player = new Player(0xfcf8ec, 10, {x:0, y:0});
+setupControls();
+setInterval(gameLoop, 1000/60);
