ff21cda1f3836298a5bb924fb2030eebd014a236
he00741098 <86698067+he00741098@users.noreply.github.com>
2023-02-27 23:02:35 +0000

    added another js file

---
+++ client/obstacles.js
@@ -0,0 +1,17 @@
+class obstacle{
+
+constructor(x, y, height, width){
+    //xy = lower left corner of obstacle
+this.x = x;
+this.y = y;
+this.height = height;
+this.width = width;
+}
+
+function isColliding(){
+
+
+    
+}
+
+}
