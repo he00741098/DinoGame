1cf6b3cb2986d06ad50da702c2af0559e3151630
DebianProgrammer <34977208+DebianProgrammer@users.noreply.github.com>
2023-02-27 15:09:58 -0800

    formatting

--- client/obstacles.js
+++ client/obstacles.js
@@ -1,17 +1,12 @@
 class obstacle{
+    constructor(x, y, height, width) {
+        //xy = lower left corner of obstacle
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+    }
 
-constructor(x, y, height, width){
-    //xy = lower left corner of obstacle
-this.x = x;
-this.y = y;
-this.height = height;
-this.width = width;
-}
-
-isColliding(player){
-
-
-
-}
-
+    isColliding(player) {
+    }
 }
