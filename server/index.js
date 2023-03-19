2bc6389685efa89ec3e4e2a9e3db0d37a6239df7
he00741098 <86698067+he00741098@users.noreply.github.com>
2023-03-19 01:06:22 +0000

    attempt #2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,62 @@
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+//const { createServer } = require("http");
+//const { Server } = require("socket.io");
 var port = port = process.env.PORT || 8125;
 //let ids = 0;
 let scores = {};
 
-const httpServer = require('http').createServer();
+const httpServer = require('http').createServer(function (request, response) {
+  console.log('request starting...');
+
+  var filePath = './client/' + request.url;
+  console.log(filePath);
+  if (filePath == './client//') {
+      filePath = './client//index.html';
+  }
+  filePath = filePath.split("?")[0];
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+      case '.js':
+          contentType = 'text/javascript';
+          break;
+      case '.css':
+          contentType = 'text/css';
+          break;
+      case '.json':
+          contentType = 'application/json';
+          break;
+      case '.png':
+          contentType = 'image/png';
+          break;      
+      case '.jpg':
+          contentType = 'image/jpg';
+          break;
+      case '.wav':
+          contentType = 'audio/wav';
+          break;
+  }
+
+  fs.readFile(filePath, function(error, content) {
+      if (error) {
+          if(error.code == 'ENOENT'){
+              fs.readFile('./404.html', function(error, content) {
+                  response.writeHead(404, { 'Content-Type': contentType });
+                  response.end(content, 'utf-8');
+              });
+          }
+          else {
+              response.writeHead(500);
+              response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+              response.end(); 
+          }
+      }
+      else {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+      }
+  });
+
+});
 //createServer();
 const io = require('socket.io')(httpServer);
 // new Server(httpServer, {
@@ -86,57 +138,5 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.createServer(function (request, response) {
-  console.log('request starting...');
-
-  var filePath = './client/' + request.url;
-  console.log(filePath);
-  if (filePath == './client//') {
-      filePath = './client//index.html';
-  }
-  filePath = filePath.split("?")[0];
-  var extname = path.extname(filePath);
-  var contentType = 'text/html';
-  switch (extname) {
-      case '.js':
-          contentType = 'text/javascript';
-          break;
-      case '.css':
-          contentType = 'text/css';
-          break;
-      case '.json':
-          contentType = 'application/json';
-          break;
-      case '.png':
-          contentType = 'image/png';
-          break;      
-      case '.jpg':
-          contentType = 'image/jpg';
-          break;
-      case '.wav':
-          contentType = 'audio/wav';
-          break;
-  }
-
-  fs.readFile(filePath, function(error, content) {
-      if (error) {
-          if(error.code == 'ENOENT'){
-              fs.readFile('./404.html', function(error, content) {
-                  response.writeHead(404, { 'Content-Type': contentType });
-                  response.end(content, 'utf-8');
-              });
-          }
-          else {
-              response.writeHead(500);
-              response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-              response.end(); 
-          }
-      }
-      else {
-          response.writeHead(200, { 'Content-Type': contentType });
-          response.end(content, 'utf-8');
-      }
-  });
-
-}).listen(port);
+httpServer.listen(port);
 console.log('Server running at http://127.0.0.1:'+port);
