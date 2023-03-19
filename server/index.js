903cb4dbf46220fd67113c1c1e212f98a1dfed68
he00741098 <86698067+he00741098@users.noreply.github.com>
2023-03-19 01:02:19 +0000

    merging server files

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-
+var port = port = process.env.PORT || 8125;
 //let ids = 0;
 let scores = {};
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://127.0.0.1:8125"
-  }
-});
+const httpServer = require('http').createServer();
+//createServer();
+const io = require('socket.io')(httpServer);
+// new Server(httpServer, {
+//   cors: {
+//     origin: "http://127.0.0.1:8125"
+//   }
+// });
 
 io.on("connection", (socket) => {
   //ids = ids+1;
@@ -84,4 +86,57 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(6969);
+httpServer.createServer(function (request, response) {
+  console.log('request starting...');
+
+  var filePath = './client/' + request.url;
+  console.log(filePath);
+  if (filePath == './client//') {
+      filePath = './client//index.html';
+  }
+  filePath = filePath.split("?")[0];
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+      case '.js':
+          contentType = 'text/javascript';
+          break;
+      case '.css':
+          contentType = 'text/css';
+          break;
+      case '.json':
+          contentType = 'application/json';
+          break;
+      case '.png':
+          contentType = 'image/png';
+          break;      
+      case '.jpg':
+          contentType = 'image/jpg';
+          break;
+      case '.wav':
+          contentType = 'audio/wav';
+          break;
+  }
+
+  fs.readFile(filePath, function(error, content) {
+      if (error) {
+          if(error.code == 'ENOENT'){
+              fs.readFile('./404.html', function(error, content) {
+                  response.writeHead(404, { 'Content-Type': contentType });
+                  response.end(content, 'utf-8');
+              });
+          }
+          else {
+              response.writeHead(500);
+              response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+              response.end(); 
+          }
+      }
+      else {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+      }
+  });
+
+}).listen(port);
+console.log('Server running at http://127.0.0.1:'+port);
